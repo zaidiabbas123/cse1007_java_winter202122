2c45c9d234a60181c19d2bb18d90c5f50c7fe31e
Anthoniraj Amalanathan <aanthoniraj@vit.ac.in>
2022-02-08 06:19:42 +0530

    Java Programs Modified based on OOP Concepts

---
+++ corejava/src/org/vit/corejava/oop/Composition.java
@@ -0,0 +1,25 @@
+package org.vit.corejava.oop;
+
+/**
+ * Author: Anthoniraj Amalanathan
+ * Topic: Association 
+ * Type Composition :  Two object are related with "has a" relation
+ * Example: Library has number of Books
+ * Objects: Library and Book
+ * Syntax: 
+ * class Library{
+ *    private Book[] books;
+ * }
+ * Sample Books Details
+ * ("Java Generics and Collections", "Maurice Naftalin", "978-8184042160")
+ * ("Java: The Complete Reference","Herbert Schildt", "978-1259589331")
+ * ("Java How to Program: Early Objects", "Harvey Deitel", "978-9353062033")
+ * ("Programming with Java","E Balagurusamy", "978-9353162344")
+ * ("Head First Java","Kathy Sierra", "978-8173666025") 
+ */
+
+public class Composition {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ corejava/src/org/vit/corejava/oop/Overloading.java
@@ -0,0 +1,28 @@
+package org.vit.corejava.oop;
+
+/**
+ * Author: Anthoniraj Amalanathan
+ * Topic: Polimorphism (Many Forms)- Same object act in different ways 
+ * - Two Types 
+ *  1. Method Overloading (Compile Time) - Same method and different parameters/types 
+ *      E.g.  register(c1,c2);  register(c1,c2,c3);
+ *  2. Method Overriding (RunTime) - Same method and parameters/types
+ *     - Used in Inheritance - Parant and Child Classes
+ *      E.g.  getCredits();  getCredits();
+ */
+
+ /**
+ * Overloading Example - Compile Time Polimorphism
+ * -Base Class: Student
+ * -Derived Class: PGStudent
+ * Within Same class methods can be overloaded
+ * With Base Class and Derived classes methods can be overloaded
+ */
+
+
+public class Overloading {
+    public static void main(String[] args) {
+  
+        
+    }
+}
