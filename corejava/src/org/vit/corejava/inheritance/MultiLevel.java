bbb96de862203479e9a6053e1df758daba1ad348
Anthoniraj Amalanathan <aanthoniraj@vit.ac.in>
2022-02-10 05:11:07 +0530

    definition updated

--- corejava/src/org/vit/corejava/inheritance/MultiLevel.java
+++ corejava/src/org/vit/corejava/inheritance/MultiLevel.java
@@ -5,6 +5,7 @@ package org.vit.corejava.inheritance;
  * Inheritance: : Getting Property from Parent
  *   - Parent -> Parent Class / Base Class / Super Class
  *   - Children -> Child Class  / Derived Class / Sub Class
+ * 
  * Types
  *   - Single Inheritance (A->B)
  *   - Multilevel Inheritance (A->B->C)
@@ -12,11 +13,16 @@ package org.vit.corejava.inheritance;
  *   - Hybrid Inheritance (A->B, B->C, C->D, A->E) 
  * Keyword Used in Java: extends
  * When to use: If the objects are coming under is-a relationship
+ * 
  * Syntax for Multilevel Inheritance (A->B->C)
- *   PErson -> Employee -> Manager
+ *   Person -> Employee -> Manager
  *   class Person {}
  *   class Employee extends Person {}
  *   class Manager extends Employee {}
+ * 
+ * Java Cosmic Super Class
+ * toString() Method Override
+ * Super Keyword  
  */
 
 
