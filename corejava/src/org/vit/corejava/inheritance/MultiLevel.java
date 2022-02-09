7035ae601b540e8be3f909ed8253dc8078383379
Anthoniraj Amalanathan <aanthoniraj@vit.ac.in>
2022-02-10 05:04:23 +0530

    Multilevel Inheritance Added

---
+++ corejava/src/org/vit/corejava/inheritance/MultiLevel.java
@@ -0,0 +1,27 @@
+package org.vit.corejava.inheritance;
+
+/**
+ * Author: Anthoniraj Amalanathan
+ * Inheritance: : Getting Property from Parent
+ *   - Parent -> Parent Class / Base Class / Super Class
+ *   - Children -> Child Class  / Derived Class / Sub Class
+ * Types
+ *   - Single Inheritance (A->B)
+ *   - Multilevel Inheritance (A->B->C)
+ *   - Hierarchical Inheritance (A->B, A->C, A->D) 
+ *   - Hybrid Inheritance (A->B, B->C, C->D, A->E) 
+ * Keyword Used in Java: extends
+ * When to use: If the objects are coming under is-a relationship
+ * Syntax for Multilevel Inheritance (A->B->C)
+ *   PErson -> Employee -> Manager
+ *   class Person {}
+ *   class Employee extends Person {}
+ *   class Manager extends Employee {}
+ */
+
+
+public class MultiLevel {
+    public static void main(String[] args) {
+        
+    }
+}
